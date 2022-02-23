9aba629d8b5ee90a32fdd3885f5b13b2a246ffd8
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-02-22 21:16:48 -0600

    Add styling and additional functionality for new styling.

--- popup.js
+++ popup.js
@@ -1,50 +1,66 @@
+// Set the user preferred line height for paragraphs, spans, list items, and
+// even YouTube comments.
 // The body of this function will be executed as a content script inside the
 // current page.
-function setLineHeight() {
+function setLineHeight(rangeInput) {
     // Nesting this helper function declaration is necessary for chrome scripting scope,
     // since setLineHeight is called from a content script and I don't know enough about
     // chrome extension content scripts yet.
     function adjustStyle(element, lineHeight) {
         element.style.lineHeight = lineHeight
-        element.style.transition = 'all 0.5s ease-in-out'
+        element.style.transition = 'all 0.3s ease-in-out'
     }
-    chrome.storage.sync.get("lineHeight", ({ lineHeight }) => {
-        const remValue = `${lineHeight}rem`
-        // YouTube comments are a bit more squished than normal paragraphs,
-        // so we'll increase the line height by a greater factor.
-        const heightFactor = 1.5
-        const youTubeRemValue = `${lineHeight * heightFactor}rem`
 
-        const paragraphs = document.querySelectorAll('p')
-        const spans = document.querySelectorAll('span')
-        const listItems = document.querySelectorAll('li')
+    const remValue = `${rangeInput}rem`
+    // YouTube comments are a bit more squished than normal paragraphs,
+    // so we'll increase the line height by a greater factor.
+    const heightFactor = 1.5
+    const youTubeRemValue = `${rangeInput * heightFactor}rem`
 
-        const youTubeComments = document.querySelectorAll('yt-formatted-string')
+    const paragraphs = document.querySelectorAll('p')
+    const spans = document.querySelectorAll('span')
+    const listItems = document.querySelectorAll('li')
+    const youTubeComments = document.querySelectorAll('yt-formatted-string')
+    const normalElements = [paragraphs, spans, listItems]
 
-        const normalElements = [paragraphs, spans, listItems]
-        for (const query of normalElements) {
-            for (const element of query) {
-                adjustStyle(element, remValue)
-            }
+    for (const query of normalElements) {
+        for (const element of query) {
+            adjustStyle(element, remValue)
         }
+    }
 
-        if (youTubeComments !== null) {
-            for (const comment of youTubeComments) {
-                adjustStyle(comment, `${youTubeRemValue}rem`)
-            }
+    if (youTubeComments !== null) {
+        for (const comment of youTubeComments) {
+            adjustStyle(comment, youTubeRemValue)
         }
-    });
+    }
 }
 
-// Initialize button with user's preferred line height.
-let btn = document.getElementById("btn-line-height");
+function showSliderValue() {
+    const rangeBullet = document.getElementById("rs-bullet");
+    const bgText = document.querySelector(".container");
+    const movingColorHSL = `hsl(${rangeInput.value * 8}, 100%, 50%)`
 
-// When the button is clicked, inject setLineHeight into current page elements.
-btn.addEventListener("click", async () => {
+    rangeBullet.innerHTML = rangeInput.value;
+    const bulletPosition = (rangeInput.value / rangeInput.max) * 7;
+    rangeBullet.style.left = `${bulletPosition}rem`;
+
+    bgText.style.color = movingColorHSL;
+    rangeBullet.style.color = movingColorHSL;
+}
+
+
+// Initialize button with user's preferred line height.
+let rangeInput = document.getElementById("line-height-range")
+
+rangeInput.addEventListener("input", showSliderValue, false);
+rangeInput.addEventListener("mouseup", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+    let value = document.getElementById("line-height-range").value
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: setLineHeight,
+        args: [value],
     });
 });
